c28928e2587284ab3209a17f218ef4ef73990677
Helentsai <pupu.her@hotmail.com>
2020-04-26 21:02:33 -0400

    code frist person

---
+++ Assets/Scripts/MouseScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Vector2 mouseLook;
+    //Vector2 smoothV;
+    //public float lookatspeed = 5.0f;
+    //public float smoothing = 2.0f;
+
+    //GameObject charactor;
+    float speed = 5.0f ;
+    public Transform target;
+
+    void Start()
+    {
+        //charactor = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Camera.main.transform.position;
+        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
+
+        Vector3 direction = target-pos;
+
+        //Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        //transform.rotation = rotation;
+        Quaternion lookRotation = Quaternion.LookRotation(direction,Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * 10.0f);
+
+        //var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        //md = Vector2.Scale(md, new Vector2(lookatspeed * smoothing, lookatspeed * smoothing));
+        //smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        //smoothV.y = Mathf.Lerp(smoothV.y, md.x, 1f / smoothing);
+        //mouseLook += smoothV;
+
+        //transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        //charactor.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, charactor.transform.up);
+
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    
+    
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moving player by keyborad
+        if (Input.GetKey(KeyCode.W))
+        {
+           transform.Translate(Vector3.forward * 5.0f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * -5.0f * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * 5.0f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * -5.0f * Time.deltaTime);
+        }
+
+        //moving direction by mouse
+        Vector3 lookdirection = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        Quaternion rotation = Quaternion.LookRotation(lookdirection, Vector3.up);
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+       
+    }
+}
